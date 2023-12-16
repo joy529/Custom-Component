2c033d0fea0adad7abfc74a9818e97a91fb33280
joy jain <joyshah204@gmial.com>
2023-12-16 23:17:53 +0530

    Initial Commit

---
+++ Custom Component/Custom_ComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_ComponentApp.swift
+//  Custom Component
+//
+//  Created by JOY JAIN on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Custom_ComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
